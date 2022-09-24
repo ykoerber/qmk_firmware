fa8e446e9989c0fea33fa8fc84d95f36e95c3bb8
Yannick Körber <ykkoer@gmail.com>
2022-09-24 16:45:05 +0200

    (untested) layout similar to my main zmk layout

---
+++ keyboards/crkbd/keymaps/ykoerber/rules.mk
@@ -0,0 +1,9 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+CAPS_WORD_ENABLE = yes
+COMMAND_ENABLE = no
+COMBO_ENABLE = no
+TAP_DANCE_ENABLE = yes
