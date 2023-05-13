24064e9fd49f022f7681ae05ff6c17dfa5d33284
Yannick Körber <ykkoer@gmail.com>
2023-05-13 16:05:43 +0200

    moved home row mods back to layer 0

--- keyboards/crkbd/keymaps/ykoerber/keymap.c
+++ keyboards/crkbd/keymaps/ykoerber/keymap.c
@@ -80,11 +80,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
    OSM(MOD_LSFT), KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  OSM(MOD_RSFT),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_BSPC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_ENTER,
+      KC_BSPC, GUI_T(KC_A),  ALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F), KC_G,            KC_H,    SFT_T(KC_J),  CTL_T(KC_K),  ALT_T(KC_L), GUI_T(KC_SCLN), KC_ENTER,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TAB,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, OSL(UTIL_LAYER),  KC_DEL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                 PTT,   LT(CHAR_MODS, KC_SPC),  LT(NAV, KC_ESC),            OSL(NUM),   OSL(SYM_L), OSL(INTJ)
+                                 PTT,   KC_SPC,  LT(NAV, KC_ESC),            OSL(NUM),   OSL(SYM_L), OSL(INTJ)
                                       //`--------------------------'  `--------------------------'
 
   ),
