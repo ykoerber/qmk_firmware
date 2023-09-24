f17c25357fd8217dae588640fa9d5dc7c833699a
Yannick Körber <ykkoer@gmail.com>
2023-09-24 13:05:01 +0200

    .

--- keyboards/crkbd/keymaps/ykoerber/keymap.c
+++ keyboards/crkbd/keymaps/ykoerber/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TAB,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, OSL(UTIL_LAYER),  KC_DEL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                 PTT,   KC_SPC,  LT(NAV, KC_ESC),            OSL(NUM),   OSL(SYM_L), OSL(INTJ)
+                                 PTT,   KC_SPC,  LT(NAV, KC_ESC),            OSL(NUM),   OSL(SYM_L), OSM(MOD_LCTL | MOD_LALT)
                                       //`--------------------------'  `--------------------------'
 
   ),
