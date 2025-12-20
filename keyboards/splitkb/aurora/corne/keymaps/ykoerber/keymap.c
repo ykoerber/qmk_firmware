65a1729fd7d02f2460fea2ace118150f903fff13
Yannick Körber <y.koerber@eurodata.de>
2025-12-20 17:10:16 +0100

    feat: added caps_word_press_user, copy paste from getreuer 😀

--- keyboards/splitkb/aurora/corne/keymaps/ykoerber/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/ykoerber/keymap.c
@@ -401,3 +401,25 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
                        '*', '*', '*',  '*', '*', '*'
     );
+
+
+bool caps_word_press_user(uint16_t keycode) {
+  switch (keycode) {
+    // Keycodes that continue Caps Word, with shift applied.
+    case KC_A ... KC_Z:
+      add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to the next key.
+      return true;
+
+    // Keycodes that continue Caps Word, without shifting.
+    case KC_1 ... KC_0:
+    case KC_BSPC:
+    case KC_DEL:
+    // I have a dedicated underscore key, so no need to shift KC_MINS.
+    case KC_MINS:
+    case KC_UNDS:
+      return true;
+
+    default:
+      return false;  // Deactivate Caps Word.
+  }
+}
