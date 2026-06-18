c6205a75a9e46c5860abccb260d196e301e6c1e6
Yannick Schwarz <y.schwarz@eurodata.de>
2026-06-18 15:42:51 +0200

    gaming layer

--- keyboards/crkbd/keymaps/ykoerber/keymap.c
+++ keyboards/crkbd/keymaps/ykoerber/keymap.c
@@ -24,6 +24,8 @@ enum custom_keycodes {
 #define MAC_NAV 8
 #define NUM 9
 #define MAC_NUM 10
+#define GAMING 11
+#define GAMING_NUM 12
 
 
 #define PTT KC_F16
@@ -55,6 +57,7 @@ const uint16_t PROGMEM fj_combo[] = {HR_F, HR_J, COMBO_END};
 const uint16_t PROGMEM mouse_combo[] = {HR_F, KC_G, COMBO_END};
 const uint16_t PROGMEM switch_to_pc_combo[] = {KC_C, LT(MAC_SYM, KC_V), KC_B, COMBO_END};
 const uint16_t PROGMEM switch_to_mac_combo[] = {KC_N, KC_M, COMMA_AND_QUEST_MARK, COMBO_END};
+const uint16_t PROGMEM gaming_combo[] = {KC_T, KC_G, KC_B, COMBO_END};
 
 
 combo_t key_combos[] = {
@@ -64,7 +67,8 @@ combo_t key_combos[] = {
     COMBO(fj_combo, CW_TOGG),
     COMBO(mouse_combo, TO(MAC_MOUSE_LAYER)),
     COMBO(switch_to_pc_combo, PDF(DEFAULT)),
-    COMBO(switch_to_mac_combo, PDF(MAC_DEFAULT))
+    COMBO(switch_to_mac_combo, PDF(MAC_DEFAULT)),
+    COMBO(gaming_combo, PDF(GAMING))
 };
 
 
@@ -154,6 +158,22 @@ KC_MPRV, KC_MNXT,       KC_7,        KC_8,        KC_9,        _______,        _
 UG_HUEU, CTL_T(KC_0),   ALT_T(KC_4),    GUI_T(KC_5),    SFT_T(KC_6),  KC_DOT,        _______, SFT_T(KC_F4),  GUI_T(KC_F5),   ALT_T(KC_F6),   CTL_T(KC_F11),  KC_VOLU,
 UG_TOGG, KC_MPLY,       KC_1,        KC_2,        KC_3,     KC_COMM   ,        _______, KC_F1,         KC_F2,          KC_F3,          KC_F12,         KC_VOLD,
                                         _______,        _______,        _______,        _______, _______,       _______
+),
+
+//====================================================================================================================================================================================
+
+[GAMING] = LAYOUT_split_3x6_3(
+  KC_TAB, KC_Q, KC_W, KC_E,    KC_R,   KC_T,                              KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_BSPC,
+  KC_LCTL, KC_A, KC_S, KC_D,    KC_F,   KC_G,                              KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_ENT,
+  KC_LSFT,  KC_Z, KC_X, KC_C,    KC_V,   KC_B,                              KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_ESC,
+                       KC_LALT, KC_SPC, OSL(GAMING_NUM),                   KC_QUOT,  _______, PDF(DEFAULT)
+),
+
+[GAMING_NUM] = LAYOUT_split_3x6_3(
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       _______, _______, _______, _______, _______, _______,
+  _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                       _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
+                             _______, _______, _______,                    _______, _______, _______
 )
 
   //====================================================================================================================================================================================
@@ -331,6 +351,14 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
                        '*', '*', '*',  '*', '*', '*'
     );
+
+
+bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
+    if (get_highest_layer(default_layer_state) == GAMING) {
+        return false;
+    }
+    return true;
+}
 
 
 bool caps_word_press_user(uint16_t keycode) {
