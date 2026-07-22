44794f010ce12a2b2e8a6d9d37e02a72c94828a6
Yannick Schwarz <y.schwarz@eurodata.de>
2026-07-22 16:27:41 +0200

    update

--- keyboards/crkbd/keymaps/ykoerber/keymap.c
+++ keyboards/crkbd/keymaps/ykoerber/keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYM] = LAYOUT_split_3x6_3(
 _______,  KC_PIPE,            KC_AMPR,           KC_LPRN,         KC_RPRN,         KC_DLR,         KC_EXLM, KC_PERC,       KC_PPLS,        KC_ASTR,        KC_CIRC,        _______,
 _______,  GUI_T(KC_QUOT),      ALT_T(KC_SLSH),    CTL_T(KC_LBRC),  SFT_T(KC_RBRC),  KC_DQUO,        KC_QUES, SFT_T(KC_EQL), CTL_T(KC_MINS), ALT_T(KC_BSLS), GUI_T(KC_GRV),  _______,
-_______,  KC_TILD,            KC_UNDS,           KC_LCBR,         KC_RCBR,         KC_HASH,        ARROW, KC_AT,         KC_LT,          KC_GT,          KC_COLN,        _______,
+_______,  KC_TILD,            KC_UNDS,           KC_LCBR,         KC_RCBR,         KC_HASH,        ARROW,  KC_LT,          KC_GT,     KC_AT,     KC_COLN,        _______,
                                                 _______,        _______,        MO(NUM),        _______, _______,       _______
 ),
 //====================================================================================================================================================================================
